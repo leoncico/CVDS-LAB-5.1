1cbe69d2744d0ed582b8ae83aff368d6219684e6
= <=>
2024-03-06 23:51:09 -0500

    Correccion final

--- SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
+++ SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @RestController
@@ -20,6 +22,7 @@ public class SpringBootHelloWorldApplication {
 	}
 
 
+	@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", methods = {RequestMethod.GET})
 	@RequestMapping("/new")
 	public String newHome(){
 		return "<strong>Leo y camilo hola mundo</strong> <br>" +
